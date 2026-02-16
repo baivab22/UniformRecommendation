c99d887c1a8ac072f4db0a4558bb281cd30f6668
baivab22 <bidaribaivab7@gmail.com>
2026-02-16 16:51:45 +0545

    push frontend only (exclude backend)

--- backend/src/models/Batch.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const batchSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  college_id: { type: mongoose.Schema.Types.ObjectId, ref: 'College', required: true },
-}, { timestamps: true });
-
-export default mongoose.model('Batch', batchSchema);
--- backend/src/models/School.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-const schoolSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-}, { timestamps: true });
-
-export default mongoose.model('School', schoolSchema);
